c8a6b2bb8a26a112a85f64525fa9e02daf99d2ba
Kyle <kylevance801@gmail.com>
2022-09-21 09:09:35 -0600

    createTeam built, readme generated using my generator

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Team-Profile-Generator
+# Team Profile Generator
+
+  [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+
+## Description
+This application is ran from the terminal using node.js. It is designed to be able to quickly and easily build a team and display their information in one place. It also uses Jest to allow testing of the constructors. 
+
+---
+
+## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributions](#contributions)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+
+  --- 
+
+## Installation 
+Clone the repository and open in visual studio code. From the terminal, run "npm i". From the terminal, run "node index.js". You will need to have node.js for this application to run.
+
+---
+
+## Usage 
+After you have built your team in the terminal, the application will build an index.html file under /dist that you can view.
+
+---
+
+## Contributions
+You are free to do with this as you please.
+
+---
+
+## Tests
+npm start, node index.js, npm test
+
+---
+
+## License
+This projected is licensed under [Unlicense] (https://unlicense.org/).
+
+---
+
+## Questions
+Feel free to reach out to me at kylevance801@gmail.com if you have any questions about this project, any other project I have worked on, or any other questions. My work can be found at https://github.com/kyle-vance/.
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -2,11 +2,62 @@
 const teamArray = [];
 // Function that takes the inputted data and forms into readable HTML to display TODO
 const createTeam = (data) => {
-}
+    // Manger
+    const createManager = data => {
+        let managerCard = `<div class="card">
+              <ul class="list-group">
+              <li class="list-group-item name">${data.name}</li>
+                <li class="list-group-item role">Role: Manager</li>
+                <li class="list-group-item">Employee ID: ${data.id}</li>
+                <li class="list-group-item">Email: <span><a href="mailto:${data.email}">${data.email}</a></span></li>
+                <li class="list-group-item">Office Phone Number: ${data.officeNo}</li>
+              </ul>
+            </div>`;
+        teamArray.push(managerCard);
+    }
+    // Engineer
+    const createEngineer = data => {
+        let engineerCard = `<div class="card">
+              <ul class="list-group">
+                <li class="list-group-item name">${data.name}</li>
+                <li class="list-group-item">Role: Engineer</li>
+                <li class="list-group-item">Employee ID: ${data.id}</li>
+                <li class="list-group-item">Email: <span id="email"><a href="mailto:${data.email}">${data.email}</a></span></li>
+                <li class="list-group-item">GitHub Username: <a target="_blank" href="https://github.com/${data.github}">${data.github}</a></li>
+              </ul>
+            </div>`;
+        teamArray.push(engineerCard);
+    }
+    // Intern
+    const createIntern = data => {
+        let internCard = `<div class="card">
+              <ul class="list-group">
+                <li class="list-group-item name">${data.name}</li>
+                <li class="list-group-item">Role: Intern</li>
+                <li class="list-group-item">Employee ID: ${data.id}</li>
+                <li class="list-group-item">Email: <span id="email"><a href="mailto:${data.email}">${data.email}</a></span></li>
+                <li class="list-group-item">School: ${data.school}</li>
+              </ul>
+            </div>`;
+        teamArray.push(internCard);
+    }
+    // Assigns the correct role based on the getRole() from the constructor
+    for (role of data) {
+        if (role.getRole() === "Manager") {
+            createManager(role);
+        }
+        if (role.getRole() === "Engineer") {
+            createEngineer(role);
+        }
+        if (role.getRole() === "Intern") {
+            createIntern(role);
+        }
+    }
+    return teamArray.join('');
+};
 // Creates the HTML document template
 function generateHTML(data) {
-    return `
-<!DOCTYPE html>
+    return `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
